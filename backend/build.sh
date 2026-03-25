e3fbadaf2c18e39fcabe7b9e503de8cb6ae52c40
cavy111 <calvindube.cd@gmail.com>
2026-03-25 03:58:52 +0200

    chore: production config for Render deployment

--- backend/build.sh
+++ backend/build.sh
@@ -4,4 +4,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py create_default_superuser
-python manage.py seed_demo_data
+python manage.py create_test_guardian
